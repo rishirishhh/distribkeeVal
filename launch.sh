d5a0c0211b706727d30e9d0d8d415f26ea49f2e3
rishirishhh <rishikavya19@gmail.com>
2025-04-22 01:03:03 +0530

    made new launch.sh to enable debugging via delve

--- launch.sh
+++ launch.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
 
-trap 'killall distribkv; rm -f *.db' SIGINT
+trap 'killall distribkv dlv; rm -f *.db' SIGINT
 
 cd $(dirname $0)
 
-killall distribkv || true
+killall distribkv dlv || true
 sleep 0.1
 
-go install -v
+# Build the binary explicitly
+go build -gcflags="all=-N -l" -o distribkv
 
-distribkv -db-location=moscow.db -http-addr=127.0.0.1:8080 -config-file=sharding.toml -shard=Moscow &
-distribkv -db-location=minsk.db -http-addr=127.0.0.1:8081 -config-file=sharding.toml -shard=Minsk &
-distribkv -db-location=kiev.db -http-addr=127.0.0.1:8082 -config-file=sharding.toml -shard=Kiev &
-distribkv -db-location=tashkent.db -http-addr=127.0.0.1:8083 -config-file=sharding.toml -shard=Tashkent &
+# Run one instance with delve
+dlv exec ./distribkv --headless --listen=:2345 --api-version=2 --log -- \
+    -db-location=moscow.db -http-addr=127.0.0.1:8080 -config-file=sharding.toml -shard=Moscow &
+
+# Run other instances normally
+./distribkv -db-location=minsk.db -http-addr=127.0.0.1:8081 -config-file=sharding.toml -shard=Minsk &
+./distribkv -db-location=kiev.db -http-addr=127.0.0.1:8082 -config-file=sharding.toml -shard=Kiev &
+./distribkv -db-location=tashkent.db -http-addr=127.0.0.1:8083 -config-file=sharding.toml -shard=Tashkent &
 
 wait
